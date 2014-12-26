5f8eb0557f3d61e651a671616c3e1bf3f50b48e5
f2 <f2@Lab-Zhong.(none)>
2014-12-26 13:35:19 +0800

    1

--- test.cpp
+++ test.cpp
@@ -19,7 +19,7 @@
 #include <eigen3/Eigen/Geometry>
 
 #include <fstream>  //save data lligen added
-
+using namespace std;
 int
 main (int argc, char** argv)
 {
@@ -45,39 +45,9 @@ main (int argc, char** argv)
   pcl::PointCloud<pcl::PointXYZ>::Ptr cloudXYZ2Ptr(&cloudXYZ2);
   pcl::fromPCLPointCloud2 (cloud2, cloudXYZ2);
 
-  // Executing the transformation
-  Eigen::Matrix4f transform;
-  transform <<0.570647,	0.074308,	0.817820,	0.054933,  //rotate -90 ,"+" anti-wiseclock
-              0.091349,	0.983966,	-0.153145,	0.167663,
-             -0.816093,	0.162100,	0.554713,	0.057849,
-              0.000000,	0.000000,	0.000000,	1.000000;
-  /*  METHOD #2: Using a Affine3f
-    This method is easier and less error prone
-  */
-  //  Translation 0.105066 0.341642 0.163335
-  //  Rotation 0.0692543 0.511385 0.00178486 0.856555
-
-  Eigen::Affine3f transform_2 = Eigen::Affine3f::Identity();
-//  transform_2.translation() << 0.105066, 0.341642, 0.163335;
-//  transform_2.rotate (Eigen::Quaternionf (0.856555, 0.0692543, 0.511385, 0.00178486 ));
-  //transform_2.rotate (Eigen::AngleAxisf (theta, Eigen::Vector3f::UnitZ()));
-  std::ifstream fin("/home/f2/catkin_ws_h/devel/lib/rgbdslam_2kinect/test.txt",std::ios::in);
-//  char ch[255];
-//  fin.getline(ch,255-1,0);
-  fin >>transform_2.translation().x()>>transform_2.translation().y() >>transform_2.translation().z();
-  float x,y,z,w;
-  fin >>x>>y>>z>>w;
-  transform_2.rotate (Eigen::Quaternionf(w,x,y,z));
-  cout<<transform_2.matrix()<<endl;
-  fin.close();
 
 
-  pcl::PointCloud<pcl::PointXYZ> transformed;
-  pcl::PointCloud<pcl::PointXYZ>::Ptr transformed_cloud (&transformed);
-  // You can either apply transform_1 or transform_2; they are the same
-  pcl::transformPointCloud (cloudXYZ2, transformed, transform_2);
-
-
+  if(1){
 //  // Create a KD-Tree
 //  pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ>);
 //  // Output has the PointNormal type in order to store the normals calculated by MLS
@@ -101,6 +71,7 @@ main (int argc, char** argv)
 //  ne.setNormalSmoothingSize(10.0f);
 //  ne.setInputCloud(cloudXYZPtr);
 //  ne.compute(*normalsPtr);
+  }
 
   //plane1 parameters
   pcl::ModelCoefficients coefficients;
@@ -168,58 +139,81 @@ main (int argc, char** argv)
   pcl::PointCloud<pcl::PointXYZ>  cloud_p2, cloud_f2;
   pcl::PointCloud<pcl::PointXYZ>::ConstPtr cloud_p2Ptr(&cloud_p2);
   pcl::ExtractIndices<pcl::PointXYZ> extract2;
-  int j = 0, nr_points2 = (int) transformed.points.size ();
-  // While 30% of the original cloud is still there
-  while (transformed.points.size () > 0.5 * nr_points2)
-  {
-    // Segment the largest planar component from the remaining cloud
-    seg2.setInputCloud (transformed_cloud);
-    seg2.segment (*inliers2, coefficients2);
-    if (inliers2->indices.size () == 0)
+
+  Eigen::Affine3f transform_2 = Eigen::Affine3f::Identity();
+  pcl::PointCloud<pcl::PointXYZ> transformed;
+  pcl::PointCloud<pcl::PointXYZ>::Ptr transformed_cloud (&transformed);
+  int nr_points2;float x,y,z,w;int k = 0;
+  std::ifstream ifile("/home/f2/catkin_ws_h/devel/lib/rgbdslam_2kinect/test.txt",std::ios::in);
+  while (!ifile.eof())
     {
-      std::cerr << "Could not estimate a planar model for the given dataset." << std::endl;
-      break;
+      transform_2.setIdentity();
+        ifile >>transform_2.translation().x()>>transform_2.translation().y() >>transform_2.translation().z();
+        ifile >>x>>y>>z>>w;
+        transform_2.rotate (Eigen::Quaternionf(w,x,y,z));
+        cout<< transform_2.matrix()<<endl;
+        transformed.clear();
+        pcl::transformPointCloud (cloudXYZ2, transformed, transform_2);
+
+        nr_points2 = (int) transformed.points.size ();
+      // While 30% of the original cloud is still there
+         while (transformed.points.size () > 0.5 * nr_points2)
+          {
+            // Segment the largest planar component from the remaining cloud
+            seg2.setInputCloud (transformed_cloud);
+            seg2.segment (*inliers2, coefficients2);
+            if (inliers2->indices.size () == 0)
+            {
+              std::cerr << "Could not estimate a planar model for the given dataset." << std::endl;
+              break;
+            }
+            // Extract the inliers
+            extract2.setInputCloud (transformed_cloud);
+            extract2.setIndices (inliers2);
+            extract2.setNegative (false);
+            extract2.filter (cloud_p2);
+            // Create the filtering object
+            extract2.setNegative (true);
+            extract2.filter (cloud_f2);
+            transformed.swap (cloud_f2);
+
+          }
+         k++;
+          //std::cerr << "plane2 para is :" << coefficients2 <<std::endl;
+
+          //  // visualize normals
+            pcl::visualization::PCLVisualizer viewer("PCL Viewer");
+            viewer.addCoordinateSystem();
+            viewer.setBackgroundColor (0.0, 0.0, 0.5);
+          //  // Define R,G,B colors for the point cloud
+          //// pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> cloudXYZ_color_handler (cloudXYZPtr, 0, 255, 255);
+          //// // We add the point cloud to the viewer and pass the color handler
+          //// viewer.addPointCloud (cloudXYZPtr, cloudXYZ_color_handler, "cloud1");
+           pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> cloudXYZ2_color_handler (cloudXYZ2Ptr, 255, 0, 255);
+           viewer.addPointCloud (cloudXYZ2Ptr, cloudXYZ2_color_handler, "cloud2");
+          //// pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> transformed_handler (transformed_cloud, 255, 0, 255);
+          //// viewer.addPointCloud (transformed_cloud, transformed_handler, "transformed_cloud2");
+           pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> cloudXYZ_color_handler (cloud_pPtr, 0, 255, 255);
+           viewer.addPointCloud (cloud_pPtr, cloudXYZ_color_handler, "cloud1");
+           pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> transformed_handler2 (cloud_p2Ptr, 100, 100, 0);
+           viewer.addPointCloud (cloud_p2Ptr, transformed_handler2, "transformed_cloud22");
+
+          ////  viewer.addPointCloudNormals<pcl::PointXYZ,pcl::PointNormal>(cloudXYZPtr, mlsPtr);
+          //// viewer.addPointCloudNormals<pcl::PointXYZ,pcl::Normal>(cloudXYZPtr, normalsPtr);
+
+
+            while (!viewer.wasStopped ())
+            {
+              viewer.spinOnce ();
+            }
+
     }
-    // Extract the inliers
-    extract2.setInputCloud (transformed_cloud);
-    extract2.setIndices (inliers2);
-    extract2.setNegative (false);
-    extract2.filter (cloud_p2);
-    // Create the filtering object
-    extract2.setNegative (true);
-    extract2.filter (cloud_f2);
-    transformed.swap (cloud_f2);
-    j++;
-  }
-  std::cerr << "plane2 para is :" << coefficients2 <<std::endl;
+  cout<<k<<endl;
+  ifile.close();
 
   //angle of plane
 
-  // visualize normals
-  pcl::visualization::PCLVisualizer viewer("PCL Viewer");
-  viewer.addCoordinateSystem();
-  viewer.setBackgroundColor (0.0, 0.0, 0.5);
-  // Define R,G,B colors for the point cloud
-// pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> cloudXYZ_color_handler (cloudXYZPtr, 0, 255, 255);
-// // We add the point cloud to the viewer and pass the color handler
-// viewer.addPointCloud (cloudXYZPtr, cloudXYZ_color_handler, "cloud1");
-// pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> cloudXYZ2_color_handler (cloudXYZ2Ptr, 255, 0, 255);
-// viewer.addPointCloud (cloudXYZ2Ptr, cloudXYZ2_color_handler, "cloud2");
-// pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> transformed_handler (transformed_cloud, 255, 0, 255);
-// viewer.addPointCloud (transformed_cloud, transformed_handler, "transformed_cloud2");
- pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> cloudXYZ_color_handler (cloud_pPtr, 0, 255, 255);
- viewer.addPointCloud (cloud_pPtr, cloudXYZ_color_handler, "cloud1");
- pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> transformed_handler2 (cloud_p2Ptr, 100, 100, 0);
- viewer.addPointCloud (cloud_p2Ptr, transformed_handler2, "transformed_cloud22");
 
-//  viewer.addPointCloudNormals<pcl::PointXYZ,pcl::PointNormal>(cloudXYZPtr, mlsPtr);
-// viewer.addPointCloudNormals<pcl::PointXYZ,pcl::Normal>(cloudXYZPtr, normalsPtr);
-
-
-  while (!viewer.wasStopped ())
-  {
-    viewer.spinOnce ();
-  }
   return 0;
   // Save output
  // pcl::io::savePCDFile ("bun0-mls.pcd", mls_points);
